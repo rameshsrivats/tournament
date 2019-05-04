24b89390410cc6e87d04fd99d10db9cee38477e7
Ramesh Srivats <srivats.tech@gmail.com>
2019-05-04 11:54:37 +0530

    remove client

---
+++ build/precache-manifest.1f1ab25c72d284002f302dd3b6514495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6d8bc2b6b0ed85708ef902bd30bd65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a27c7b0fbc208cea08",
+    "url": "/static/css/2.81b134f3.chunk.css"
+  },
+  {
+    "revision": "c73d5e9ab85cd04f3673",
+    "url": "/static/css/main.6aa62de0.chunk.css"
+  },
+  {
+    "revision": "e8a27c7b0fbc208cea08",
+    "url": "/static/js/2.832e36d6.chunk.js"
+  },
+  {
+    "revision": "c73d5e9ab85cd04f3673",
+    "url": "/static/js/main.f37db290.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a49953636a2732221fde3906abf05d60",
+    "url": "/static/media/ground.a4995363.jpg"
+  }
+]);
